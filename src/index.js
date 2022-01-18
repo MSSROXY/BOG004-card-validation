544e87aeaae67c173b1efdddc0a824a264bbe7e0
mssroxy <roxysolanopino@gmail.com>
2022-01-18 13:45:14 -0300

    tarjeta y formulario, no funciona al voltear la tarjeta

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+const tarjeta = document.querySelector('#tarjeta'),
+  btnAbrirFormulario = document.querySelector('#btn-abrir-formulario'),
+  formulario = document.querySelector('#formulario-tarjeta');
+
+tarjeta.addEventListener('click', () => {
+  tarjeta.classList.toggle('active');
+});
+
+btnAbrirFormulario.addEventListener('click', () => {
+  btnAbrirFormulario.classList.toggle('active');
+  formulario.classList.toggle('active');
+});
+
 import validator from './validator.js';
 
 console.log(validator);
