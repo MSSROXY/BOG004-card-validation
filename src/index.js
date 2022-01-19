b8433a449f4c5307b685c0fb4c103d4a5a960c5f
mssroxy <roxysolanopino@gmail.com>
2022-01-18 21:06:34 -0300

    fondo ok, me funciona hasta el giro de la tarjeta

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const tarjeta = document.querySelector('#tarjeta'),
   btnAbrirFormulario = document.querySelector('#btn-abrir-formulario'),
-  formulario = document.querySelector('#formulario-tarjeta');
+  formulario = document.querySelector('#formulario-tarjeta'),
+  numeroTarjeta= document.querySelector('#tarjeta .numero'),
+  nombreTarjeta= document.querySelector('#tarjeta .nombre'),
+  logoMarca = document.querySelector('#logo-marca'),
+  firma = document.querySelector('#tarjeta .firma p'),
+  mesExpiracion = document.querySelector('#tarjeta #expiracion .mes'),
+  yearExpiracion = document.querySelector('#tarjeta #expiracion .year'),
+  ccv = document.querySelector('#tarjeta .ccv');
+
+//* para voltear la tarjeta al frente
+const mostrarfrente = () => {
+  if (tarjeta.classList.contains('active')) {
+    tarjeta.classList.remove('active');
+  }
+};
 
 // * rotacion de la tarjeta
 tarjeta.addEventListener('click', () => {
@@ -28,8 +42,85 @@ for(let i = yearActual - 8; i<= yearActual + 8; i++){
   opcion.innerText = i;
   formulario.selectYear.appendChild(opcion);
 }
+//* Input numero de Tarjeta
+formulario.inputNumero.addEventListener('keyup', (e) => {
+  let valorInput = e.target.value;
 
+  formulario.inputNumero.value = valorInput
+  //* eliminar espacios blancos
+  .replace(/\s/g, '')
+   //* eliminar espacios blancos
+  .replace(/\D/g, '')
+  //* poner espacio cada 4 numeros
+  .replace(/([0-9]{4})/g, '$1 ')
+  //* eliminar el ultimo espacio
+  .trim();
 
+  numeroTarjeta.textContent= valorInput;
+
+  if(valorInput == ''){
+    numeroTarjeta.textContent = '#### #### #### ####';
+
+    logoMarca.innerHTML = '';
+  }
+
+  if(valorInput[0] == 4){
+    logoMarca.innerHTML = '';
+    const imagen = document.createElement('img');
+    imagen.src= 'img/logos/visa.png';
+    logoMarca.appendChild(imagen);
+  }
+  else if (valorInput[0] == 5){
+    logoMarca.innerHTML = '';
+    const imagen = document.createElement('img');
+    imagen.src= 'img/logos/mastercard.png';
+    logoMarca.appendChild(imagen);
+  }
+  //voltear la tarjeta para verse el frente
+  mostrarfrente();
+});
+
+//* Input nombre de la tarjerta
+formulario.inputNombre.addEventListener('keyup', (e) => {
+  let valorInput = e.target.value;
+
+  formulario.inputNombre.value= valorInput.replace(/[0-9]/g,'');
+  nombreTarjeta.textContent= valorInput;
+  firma.textContent= valorInput;
+
+  if (valorInput =='') {
+    nombreTarjeta.textContent = 'SU NOMBRE';
+  }
+
+  mostrarfrente();
+});
+
+//* Select mes
+formulario.selectMes.addEventListener('change',(e) => {
+  mesExpiracion.textContent= e.target.value;
+  mostrarfrente();
+});
+
+//* Select year
+formulario.selectYear.addEventListener('change',(e) => {
+  yearExpiracion.textContent= e.target.value.slice(2);
+  mostrarfrente();
+});
+
+//* Select CCV
+formulario.inputCCV.addEventListener('keyup', () => {
+  if(!tarjeta.classList.contains('active')){
+    tarjeta.classList.toggle('active');
+  }
+
+  formulario.inputCCV.value = formulario.inputCCV.value
+  //* eliminar espacios
+  .replace(/\s/g,'')
+  //* eliminar letras
+  .replace(/\D/g, '');
+
+  ccv.textContent = formulario.inputCCV.value;
+});
 
 import validator from './validator.js';
 
